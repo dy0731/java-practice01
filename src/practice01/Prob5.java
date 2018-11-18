5996fd965d3d3631ac9744ac5ca3454de3993e02
nexte <nexte@LAPTOP-IB7CQKHH>
2018-11-18 21:25:53 +0900

    5번

--- src/practice01/Prob5.java
+++ src/practice01/Prob5.java
@@ -3,14 +3,27 @@ package practice01;
 public class Prob5 {
 
 	public static void main(String[] args) {
-		for(int i = 1; i < 100; i++) {
+		for (int i = 1; i < 100; i++) {
+
 			String number = String.valueOf(i);
+
 			int size = number.length();
-			
+
 			int clapCount = 0;
-			
-			for(int j = 0; j < size; j++) {
+
+			for (int j = 0; j < size; j++) {
 				char c = number.charAt(j);
+				if (c == '3' || c == '6' || c == '9') {
+					clapCount++;
+				}
+			}
+
+			if (clapCount > 0) {
+				System.out.print(i + " ");
+				for (int k = 0; k < clapCount; k++) {
+					System.out.print("짝");
+				}
+				System.out.print("\n");
 			}
 		}
 	}
